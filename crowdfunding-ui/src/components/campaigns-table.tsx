e0a29faef2763dcb069be1d307064096ec3ee598
SiddheshZagade <siddeshzagade7@gmail.com>
2023-03-03 02:07:50 +0530

    new card

--- crowdfunding-ui/src/components/campaigns-table.tsx
+++ crowdfunding-ui/src/components/campaigns-table.tsx
@@ -1,123 +1,139 @@
 import React, { useEffect, useState } from 'react';
 import { BN, Program, ProgramAccount, web3 } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Form } from 'react-bootstrap';
 
 interface CampaignsTableProps {
-    program: Program;
-    walletKey: PublicKey;
+  program: Program;
+  walletKey: PublicKey;
 }
 
 export const CampaignsTable: React.FC<CampaignsTableProps> = ({
-    program,
-    walletKey,
+  program,
+  walletKey,
 }) => {
-    const [campaigns, setCampaigns] = useState<ProgramAccount[]>([]);
+  const [campaigns, setCampaigns] = useState<ProgramAccount[]>([]);
 
-    const getAllCampaigns = async () => {
-        const campaigns = await program.account.campaign.all();
-        setCampaigns(campaigns);
-    };
+  const getAllCampaigns = async () => {
+    const campaigns = await program.account.campaign.all();
+    setCampaigns(campaigns);
+  };
 
-    useEffect(() => {
-        getAllCampaigns();
-    }, [walletKey]);
+  useEffect(() => {
+    getAllCampaigns();
+  }, [program, walletKey]);
 
-    const donate = async (campaignKey: PublicKey) => {
-        try {
-            await program.methods
-                .donate(new BN(0.2 * web3.LAMPORTS_PER_SOL))
-                .accounts({
-                    campaign: campaignKey,
-                    user: walletKey,
-                    systemProgram: web3.SystemProgram.programId,
-                })
-                .rpc();
-            await getAllCampaigns();
-        } catch (err) {
-            console.error('Donate transaction error: ', err);
-        }
-    };
+  const donate = async (campaignKey: PublicKey) => {
+    try {
+      await program.rpc.donate(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+        accounts: {
+          campaign: campaignKey,
+          user: walletKey,
+          systemProgram: web3.SystemProgram.programId,
+        },
+      });
+      await getAllCampaigns();
+    } catch (err) {
+      console.error('Donate transaction error: ', err);
+    }
+  };
 
-    const withdraw = async (campaignKey: PublicKey) => {
-        try {
-            await program.methods
-                .withdraw(new BN(0.2 * web3.LAMPORTS_PER_SOL))
-                .accounts({
-                    campaign: campaignKey,
-                    user: walletKey,
-                })
-                .rpc();
-        } catch (err) {
-            console.error('Withdraw transaction error: ', err);
-        }
-    };
+  const withdraw = async (campaignKey: PublicKey) => {
+    try {
+      await program.rpc.withdraw(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+        accounts: {
+          campaign: campaignKey,
+          user: walletKey,
+        },
+      });
+    } catch (err) {
+      console.error('Withdraw transaction error: ', err);
+    }
+  };
 
-    const allCampaigns: () => JSX.Element[] = () => {
-        return campaigns.map((c, i) => {
-            const key = c.publicKey.toBase58();
+  const allCampaigns = () => {
+    return campaigns.map((c, i) => {
+      const key = c.publicKey.toBase58();
 
-            return (
-                <Card key={key} className="m-3" style={{ width: '18rem' }}>
-                    <Card.Img
-                        variant="top"
-                        src={c.account.imageUrl}
-                        style={{ height: '10rem' }}
-                    />
+      return (
+        <Card
+          key={key}
+          className="m-3"
+          style={{
+            width: '20rem',
+            backgroundImage: 'linear-gradient(135deg, #2c2c2c 0%, #1a1a1a 100%)',
+            borderRadius: '1rem',
+            boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.15)',
+          }}
+        >
+          <Card.Img
+            variant="top"
+            src={c.account.imageUrl}
+            style={{ height: '12rem', objectFit: 'cover', borderRadius: '1rem 1rem 0 0' }}
+          />
 
-                    <Card.Body>
-                        <Card.Title>{c.account.name}</Card.Title>
-                        <Card.Text>{c.account.description}</Card.Text>
-                        <div className="flex flex-wrap">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                #campaign
-                            </span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                #donations
-                            </span>
-                        </div>
-                        <div className="mt-4 flex justify-between">
-                            <p className="font-bold text-xl mb-2">
-                                Target Amount:{' '}
-                                {c.account.targetAmount.toString()}
-                            </p>
-                            <p className="font-bold text-xl mb-2">
-                                Donated:{' '}
-                                {(
-                                    c.account.amountDonated /
-                                    web3.LAMPORTS_PER_SOL
-                                ).toString()}
-                            </p>
-                        </div>
-                        <div className="mt-4 d-grid gap-2">
-                            <Button
-                                variant="primary"
-                                onClick={() => donate(c.publicKey)}
-                            >
-                                Donate
-                            </Button>
-                            <Button
-                                variant="danger"
-                                onClick={() => withdraw(c.publicKey)}
-                                disabled={
-                                    c.account.owner.toBase58() !==
-                                    walletKey.toBase58()
-                                }
+          <Card.Body
+            style={{
+              backgroundImage: 'linear-gradient(135deg, #2c2c2c 0%, #1a1a1a 100%)',
+              borderRadius: '0 0 1rem 1rem',
+            }}
+          >
+            <Card.Title style={{ color: 'white', fontSize: '1.2rem' }}>
+              {c.account.name}
+            </Card.Title>
+            <Card.Text style={{ color: 'white', fontSize: '0.8rem' }}>
+              {c.account.description}
+            </Card.Text>
+
+            <div className="text-lg text-gray-400">
+              <p>
+                Target Amount:{' '}
+                {c.account.targetAmount.toString()}{' '}
+                <span className="text-white">
+                  ({(c.account.targetAmount / web3.LAMPORTS_PER_SOL).toString()} SOL)
+                </span>
+              </p>
+              <p>
+                Donated:{' '}
+                {(c.account.amountDonated / web3.LAMPORTS_PER_SOL).toString()}{' '}
+                <span className="text-white">
+      ({(c.account.amountDonated / web3.LAMPORTS_PER_SOL).toString()} SOL)
+    </span>
+  </p>
+</div>
+            <div className="mt-4 d-grid gap-2">
+              <Button
+                variant="primary"
+                size="sm"
+                onClick={() => donate(c.publicKey)}
+              >
+                Donate
+              </Button>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => withdraw(c.publicKey)}
+                disabled={c.account.owner.toBase58() !== walletKey.toBase58()}
                             >
                                 Withdraw
                             </Button>
                         </div>
                     </Card.Body>
+
                 </Card>
             );
         });
     };
 
     return (
-        <div className="d-flex flex-wrap justify-content-center">
-            {allCampaigns()}
+        <div className="h-screen bg-gradient-to-br from-purple-800 to-purple-600">
+          <div className="container mx-auto py-8">
+            <div className="d-flex flex-wrap justify-content-center">
+              {allCampaigns()}
+            </div>
+          </div>
         </div>
-    );
+      );
 };
 
 export default CampaignsTable;
