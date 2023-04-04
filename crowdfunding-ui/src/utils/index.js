a35e2b85c1ba79e45b4a107e7ed49f9c90483ac4
Siddhesh <siddeshzagade7@gmail.com>
2023-04-04 20:27:42 +0530

    progress bar fixed

--- crowdfunding-ui/src/utils/index.js
+++ crowdfunding-ui/src/utils/index.js
@@ -11,12 +11,3 @@ export const calculateBarPercentage = (targetAmount, donatedAmount) => {
     return percentage;
 };
 
-export const checkIfImage = (url, callback) => {
-    const img = new Image();
-    img.src = url;
-
-    if (img.complete) callback(true);
-
-    img.onload = () => callback(true);
-    img.onerror = () => callback(false);
-};
